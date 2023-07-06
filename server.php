9c378f04721f15dbad34316973ba096baad5cb54
Wiktoria Oblizajek <ob@gmail.com>
2023-07-06 12:24:05 +0200

    deleting node packages and changing server.php to get 5 habits for every day of the week

--- server.php
+++ server.php
@@ -9,16 +9,15 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
 }
+$currentDayOfWeek = date("l");
 
-$query = "SELECT * FROM `habits`ORDER BY RAND() LIMIT 5";
+$query = "SELECT * FROM `habits` WHERE `day_of_week` = '$currentDayOfWeek' LIMIT 5";
 $result = mysqli_query($conn, $query);
-$time = date("H:i");
-
 
 if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         echo "<div class='habit-and-icon'><div class='habit-container'>
-        <p class='habit'>" . $row['nazwa nawyku'] . "</p></div>" . "<div class='icon-container'><i class='icon check fas fa-check'></i> <i class='icon xmark fa-solid fa-xmark'></i></div></div>";
+        <p class='habit'>" . $row['habit_name'] . "</p></div>" . "<div class='icon-container'><i class='icon check fas fa-check'></i> <i class='icon xmark fa-solid fa-xmark'></i></div></div>";
     }
 } else {
     echo "Brak danych w bazie.";
